66d712a069a61285de69001d158502aa38c65801
JCao-CMU <zhexic@andrew.cmu.edu>
2023-03-07 21:25:44 -0500

    CRLIBM does not work...?

--- lib/trinumber.c
+++ lib/trinumber.c
@@ -3,6 +3,7 @@
 #include <float.h>
 #include <limits.h>
 #include <math.h>
+#include "../crlibm/crlibm.h"
 #include "../IGen/igen_lib/igen_lib.h"
 #include "../IGen/igen_lib/igen_dd_lib.h"
 #include "../IGen/igen_lib/igen_math.h"
@@ -28,8 +29,8 @@ interval_t create_interval(int real){
     itv.imag.interval = imag_itv;
     itv.real.center = real_center;
     itv.imag.center = img_center;
-    itv.real.interval.lo = -itv.real.interval.lo;
-    itv.imag.interval.lo = -itv.imag.interval.lo;
+    itv.real.interval.lo = itv.real.interval.lo;
+    itv.imag.interval.lo = itv.imag.interval.lo;
     itv.real.interval.up = itv.real.interval.up;
     itv.imag.interval.up = itv.imag.interval.up;
     return itv;
@@ -58,10 +59,7 @@ interval_t interval_mul(interval_t A, interval_t B) {
     interval_t result;
 
     f32_I aRbR, aCbC, aRbC, aCbR;
-    A.real.interval.lo = -A.real.interval.lo;
-    A.imag.interval.lo = -A.imag.interval.lo;
-    B.real.interval.lo = -B.real.interval.lo;
-    B.imag.interval.lo = -B.imag.interval.lo;
+
     aRbR = _ia_mul_f32(A.real.interval, B.real.interval);
     aCbC = _ia_mul_f32(A.imag.interval, B.imag.interval);
     aRbC = _ia_mul_f32(A.real.interval, B.imag.interval);
@@ -69,8 +67,6 @@ interval_t interval_mul(interval_t A, interval_t B) {
 
     result.real.interval = _ia_sub_f32(aRbR, aCbC);
     result.imag.interval = _ia_add_f32(aRbC, aCbR);
-    result.real.interval.lo = -result.real.interval.lo;
-    result.imag.interval.lo = -result.imag.interval.lo;
     
     double aRbRd, aCbCd, aRbCd, aCbRd;
     aRbRd = (A.real.center * B.real.center);
@@ -86,18 +82,14 @@ interval_t interval_mul(interval_t A, interval_t B) {
 tnum_real_t create_real(long A){
     tnum_real_t result;
     result.interval = _ia_cast_int_to_f32(A);
-    result.interval.lo = -result.interval.lo;
     result.center = (double)A;
     return result;
 }
 
 tnum_real_t real_mult(tnum_real_t A, tnum_real_t B){
-    A.interval.lo = -A.interval.lo;
-    B.interval.lo = -B.interval.lo;
     f32_I result;
     result = _ia_mul_f32(A.interval, B.interval);
     double result_c = A.center * B.center;
-    result.lo = -result.lo;
     tnum_real_t final_result;
     final_result.center = result_c;
     final_result.interval = result;
@@ -105,12 +97,9 @@ tnum_real_t real_mult(tnum_real_t A, tnum_real_t B){
 }
 
 tnum_real_t real_div(tnum_real_t A, tnum_real_t B){
-    A.interval.lo = -A.interval.lo;
-    B.interval.lo = -B.interval.lo;
     f32_I result;
     result = _ia_div_f32(A.interval, B.interval);
     double result_c = A.center / B.center;
-    result.lo = -result.lo;
     tnum_real_t final_result;
     final_result.center = result_c;
     final_result.interval = result;
@@ -118,15 +107,12 @@ tnum_real_t real_div(tnum_real_t A, tnum_real_t B){
 }
 
 interval_t complex_exp(tnum_real_t A){
-    A.interval.lo = -A.interval.lo;
     tnum_real_t cosine;
     cosine.interval = _ia_cos_f32(A.interval);
-    cosine.interval.lo = -cosine.interval.lo;
     cosine.center = cos(A.center);
 
     tnum_real_t sine;
     sine.interval = _ia_sin_f32(A.interval);
-    sine.interval.lo = -sine.interval.lo;
     sine.interval = _ia_neg_f32(sine.interval);
     sine.center = -sin(A.center);
 
@@ -140,9 +126,9 @@ interval_t complex_exp(tnum_real_t A){
 
 void print_interval(interval_t A){
     float ru = A.real.interval.up;
-    float rl = A.real.interval.lo;
+    float rl = -A.real.interval.lo;
     float iu = A.imag.interval.up;
-    float il = A.imag.interval.lo;
+    float il = -A.imag.interval.lo;
     double rc = A.real.center;
     double ic = A.imag.center;
     printf("({%f, %f, %f}, {%f, %f, %f})\n", rl, rc, ru, il, ic, iu);
@@ -150,9 +136,9 @@ void print_interval(interval_t A){
 
 bool is_valid_interval(interval_t A) {
     float ru = A.real.interval.up;
-    float rl = A.real.interval.lo;
+    float rl = -A.real.interval.lo;
     float iu = A.imag.interval.up;
-    float il = A.imag.interval.lo;
+    float il = -A.imag.interval.lo;
     double rc = A.real.center;
     double ic = A.imag.center;
 
